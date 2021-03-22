546b061e8982076ce25b69f215a665acb11441ce
Ubuntu <ChangerOfWays@gmail.com>
2021-03-22 18:35:10 +0000

    Added additional styling.

--- script.js
+++ script.js
@@ -1,18 +1,10 @@
 window.addEventListener( 'load', (click) => {
-  const skills_button = document.getElementById("skills_button");
-  skills_button.addEventListener( 'click', (click) => {
-    console.log( "click" );
-    const skills = document.getElementById("skills");
-    console.dir( skills );
-//    console.log( skills.nextSibling.style.height );
-    skills.style.display = "block";
-  });
-
-
-  const bio_button = document.getElementById("expand_bio");
-  bio_button.addEventListener( 'click', (click) => {
-    console.log( "bio" );
-    const bio_text = document.getElementById("bio_text");
-    bio_text.style.display = "block";
+  const contact_me_btn = document.getElementById("contact_me_button");
+  contact_me_btn.addEventListener( 'click', (click) => {
+    const contact_me = document.getElementById("contact_me");
+    contact_me.scrollIntoView( {
+      block: 'center',
+      behavior: 'smooth'
+    } );
   });
 });
