6fe573662e468f42942bab80d138602d72eccbbc
Ubuntu <ChangerOfWays@gmail.com>
2021-03-24 16:55:59 +0000

    Added picture galleries to all projects.

--- script.js
+++ script.js
@@ -45,6 +45,62 @@ window.addEventListener( 'load', (click) => {
     "triviacards": 0
   }
 
+  const oinker_next = document.getElementById("oinker_pic_next");
+  oinker_next.addEventListener( 'click', (click) => {
+    const oinker_image = document.getElementById("oinker_01");
+    const oinker_imageB = document.getElementById("oinker_02");
+    const oinker_imageC = document.getElementById("oinker_03");
+    const oinker_imageD = document.getElementById("oinker_04");
+    const oinker_comp = window.getComputedStyle( oinker_image );
+
+    const width = oinker_comp.getPropertyValue( 'width' ).slice(0,-2);
+    const offset = getX( oinker_image.style.transform );
+
+    let mod = (offset-width) + "px";
+    gallery.oinker++;
+    if( offset-width < width*-2 ) {
+      mod = (width*-3) + "px";
+      gallery.oinker = 3;
+    }
+
+    const effect = "translateX(" + mod + ")";
+
+    oinker_image.style.transform = effect;
+    oinker_imageB.style.transform = effect;
+    oinker_imageC.style.transform = effect;
+    oinker_imageD.style.transform = effect;
+  });
+
+  const oinker_last = document.getElementById("oinker_pic_last");
+  oinker_last.addEventListener( 'click', (click) => {
+    const oinker_image = document.getElementById("oinker_01");
+    const oinker_imageB = document.getElementById("oinker_02");
+    const oinker_imageC = document.getElementById("oinker_03");
+    const oinker_imageD = document.getElementById("oinker_04");
+    const oinker_comp = window.getComputedStyle( oinker_image );
+
+    const width = Number(
+      oinker_comp.getPropertyValue( 'width' ).slice(0,-2));
+    const offset = Number(
+      getX( oinker_image.style.transform ));
+
+
+
+    let mod = (offset+width) + "px";
+    gallery.oinker--;
+    if( offset+width > 0 ) {
+      mod = 0;
+      gallery.oinker = 0;
+    }
+
+    const effect = "translateX(" + mod + ")";
+
+    oinker_image.style.transform = effect;
+    oinker_imageB.style.transform = effect;
+    oinker_imageC.style.transform = effect;
+    oinker_imageD.style.transform = effect;
+  });
+
   const ketris_next = document.getElementById("ketris_pic_next");
   ketris_next.addEventListener( 'click', (click) => {
     const ketris_image = document.getElementById("ketris_01");
@@ -97,12 +153,72 @@ window.addEventListener( 'load', (click) => {
     ketris_imageC.style.transform = effect;
   });
 
+
+  const triviacards_next = document.getElementById("triviacards_pic_next");
+  triviacards_next.addEventListener( 'click', (click) => {
+    const triviacards_image = document.getElementById("triviacards_01");
+    const triviacards_imageB = document.getElementById("triviacards_02");
+    const triviacards_imageC = document.getElementById("triviacards_03");
+    const triviacards_imageD = document.getElementById("triviacards_04");
+    const triviacards_comp = window.getComputedStyle( triviacards_image );
+
+    const width = triviacards_comp.getPropertyValue( 'width' ).slice(0,-2);
+    const offset = getX( triviacards_image.style.transform );
+
+    let mod = (offset-width) + "px";
+    gallery.triviacards++;
+    if( offset-width < width*-2 ) {
+      mod = (width*-3) + "px";
+      gallery.triviacards = 3;
+    }
+
+    const effect = "translateX(" + mod + ")";
+
+    triviacards_image.style.transform = effect;
+    triviacards_imageB.style.transform = effect;
+    triviacards_imageC.style.transform = effect;
+    triviacards_imageD.style.transform = effect;
+  });
+
+  const triviacards_last = document.getElementById("triviacards_pic_last");
+  triviacards_last.addEventListener( 'click', (click) => {
+    const triviacards_image = document.getElementById("triviacards_01");
+    const triviacards_imageB = document.getElementById("triviacards_02");
+    const triviacards_imageC = document.getElementById("triviacards_03");
+    const triviacards_imageD = document.getElementById("triviacards_04");
+    const triviacards_comp = window.getComputedStyle( triviacards_image );
+
+    const width = Number(
+      triviacards_comp.getPropertyValue( 'width' ).slice(0,-2));
+    const offset = Number(
+      getX( triviacards_image.style.transform ));
+
+
+
+    let mod = (offset+width) + "px";
+    gallery.triviacards--;
+    if( offset+width > 0 ) {
+      mod = 0;
+      gallery.triviacards = 0;
+    }
+
+    const effect = "translateX(" + mod + ")";
+
+    triviacards_image.style.transform = effect;
+    triviacards_imageB.style.transform = effect;
+    triviacards_imageC.style.transform = effect;
+    triviacards_imageD.style.transform = effect;
+  });
+
+
+
   window.addEventListener( 'resize', (resize) => {
     console.log( "resize" );
     if( gallery.ketris != 0 ) {
       const ketris_image = document.getElementById("ketris_01");
       const ketris_imageB = document.getElementById("ketris_02");
       const ketris_imageC = document.getElementById("ketris_03");
+      const ketris_imageD = document.getElementById("ketris_04");
       const ketris_comp = window.getComputedStyle( ketris_image );
 
       const width = Number(
@@ -112,6 +228,7 @@ window.addEventListener( 'load', (click) => {
       ketris_image.style.transform = effect;
       ketris_imageB.style.transform = effect;
       ketris_imageC.style.transform = effect;
+      ketris_imageD.style.transform = effect;
     }
   });
 });
