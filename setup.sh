67f66a621f9c252afdc61183fe316bb038ffe65f
Ubuntu <ubuntu@ip-172-31-36-79.us-west-2.compute.internal>
2021-04-19 18:47:07 +0000

    Added frontend directory, put frontend data in it, added new path to Nginx setup script.

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ sudo apt-get install nginx -y
 sudo ufw allow 'Nginx HTTP'
 sudo ufw allow ssh
 sudo ufw enable
-cd /etc/nginx/sites-enabled && sudo sed -i "s/root \/var\/www\/html;/root \/home\/ubuntu\/Portfolio;/g" default
+cd /etc/nginx/sites-enabled && sudo sed -i "s/root \/var\/www\/html;/root \/home\/ubuntu\/frontend\/Portfolio;/g" default
 sudo systemctl restart nginx
 
 #HTTPS Certbot
