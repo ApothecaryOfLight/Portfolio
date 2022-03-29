b7cf02bb556c587d76b50453ba63d959453cadc6
ApothecaryOfLight <ChangerOfWays@gmail.com>
2022-03-29 14:23:13 +0000

    Reorganized directory structure.

--- frontend/loading_modal.js
+++
@@ -1,7 +0,0 @@
-function launch_loading_modal( waiting_func, callback_func ) {
-  //1) Display a transparent layer over the page. Animate.
-
-  //2) Hand promise results of waiting_func to callback_func.
-
-  //3) Hide the trasparent layer.
-}
